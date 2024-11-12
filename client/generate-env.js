053721a5dc28ff6585cb6049f425bf7cedc6c75a
albertomorales14 <cutiti007@gmail.com>
2024-11-12 10:42:32 +0100

    pro 5

--- client/generate-env.js
+++ client/generate-env.js
@@ -6,15 +6,26 @@ if (!fs.existsSync(path)) {
   fs.mkdirSync(path, { recursive: true });
 }
 
-// Lee las variables de entorno desde Vercel
 const API_URL = process.env.API_URL || 'https://securoservapp.onrender.com/';
 const PRODUCTION = process.env.PRODUCTION === 'true';
 const DARK_LAYER = process.env.DARK_LAYER;
 const HILL_SHADE_LAYER = process.env.HILL_SHADE_LAYER;
 const CLOUDINARY_API_URL = process.env.CLOUDINARY_API_URL;
 
-// Define el contenido del archivo environment.prod.ts
-const envConfigFile = `
+// Configuración del archivo environment.ts para desarrollo
+const devConfigFile = `
+  export const environment = {
+    PRODUCTION: false,
+    API_URL: 'http://localhost:5000',
+    url: 'http://localhost:5000',
+    DARK_LAYER: '${DARK_LAYER}',
+    HILL_SHADE_LAYER: '${HILL_SHADE_LAYER}',
+    CLOUDINARY_API_URL: '${CLOUDINARY_API_URL}'
+  };
+`;
+
+// Configuración del archivo environment.prod.ts para producción
+const prodConfigFile = `
   export const environment = {
     PRODUCTION: ${PRODUCTION},
     API_URL: '${API_URL}',
@@ -24,6 +35,8 @@ const envConfigFile = `
   };
 `;
 
-// Crea el archivo environment.prod.ts en la carpeta src/environments
-fs.writeFileSync('./src/environments/environment.prod.ts', envConfigFile);
-console.log('Environment file generated successfully');
+// Crear ambos archivos environment.ts y environment.prod.ts
+fs.writeFileSync('./src/environments/environment.ts', devConfigFile);
+fs.writeFileSync('./src/environments/environment.prod.ts', prodConfigFile);
+
+console.log('Environment files generated successfully');
